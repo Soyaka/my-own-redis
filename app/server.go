87524c2acd83691c32cc164f6b4f2f15dbc1a107
soyaka <soyaka@soyaka.soyaka>
2024-03-01 00:07:22 +0100

    pass 3nd stage

--- app/server.go
+++ app/server.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
+	// Uncomment this block to pass the first stage
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 	// Uncomment this block to pass the first stage
-
+	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
 
- 	conn , err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for i := 0; i < 2; i++ {
+		con, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		handleConnection(con)
 	}
-	_,  err = conn.Write([]byte("+PONG\r\n"))
-	if err!= nil{
-		fmt.Println("Error Writing Message: ", err.Error())
+}
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	buf := make([]byte, 1024)
+	for {
+		_, err := c.Read(buf)
+		if err != nil {
+			os.Exit(1)
+		}
+		_, err = c.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			os.Exit(1)
+		}
 	}
-	fmt.Println("succsusfly make a response: ")
-
 }
