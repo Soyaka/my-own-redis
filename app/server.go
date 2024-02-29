343c46ad0c711ad98e36dd96fdab4e16ca43d11e
soyaka <soyaka@soyaka.soyaka>
2024-02-29 23:18:46 +0100

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -12,7 +12,6 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -20,9 +19,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+ 	conn , err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	_,  err = conn.Write([]byte("+PONG\r\n"))
+	if err!= nil{
+		fmt.Println("Error Writing Message: ", err.Error())
+	}
+	fmt.Println("succsusfly make a response: ")
+
 }
