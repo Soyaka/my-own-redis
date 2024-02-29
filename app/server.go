a62fe08c214a1cb2059509a45d1532d2a1007e9b
soyaka <soyaka@soyaka.soyaka>
2024-03-01 00:11:49 +0100

    pass 3nd stage

--- app/server.go
+++ app/server.go
@@ -1,43 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
-	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		fmt.Println("Error:", err)
+		return
 	}
+	defer listener.Close()
 
-	for i := 0; i < 2; i++ {
-		con, err := l.Accept()
+	fmt.Println("Listening on :6379...")
+	for {
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			fmt.Println("Error:", err)
+			continue
 		}
-		handleConnection(con)
+		go handleConnection(conn)
 	}
 }
-func handleConnection(c net.Conn) {
-	defer c.Close()
-	buf := make([]byte, 1024)
-	for {
-		_, err := c.Read(buf)
-		if err != nil {
-			os.Exit(1)
-		}
-		_, err = c.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			os.Exit(1)
-		}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("Accepted connection from:", conn.RemoteAddr())
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		command := scanner.Text()
+		response := "+PONG\r\n"
+		conn.Write([]byte(response))
+		fmt.Printf("Received: %s, Sent: %s", command, response)
 	}
 }
