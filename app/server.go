cc41af3968ebab90070c437a403fead4d4f265ba
soyaka <soyaka@soyaka.soyaka>
2024-03-02 23:10:03 +0100

    the 5th stage

--- app/server.go
+++ app/server.go
@@ -64,7 +64,7 @@ func handleDecode(buff string) []string {
 func handleCommand(elements []string) string {
 	switch strings.ToLower(elements[0]) {
 	case "echo":
-		return "+" + elements[1] + "\r\n"
+		return "+" + strings.Join(elements[1:],"") + "\r\n"
 	case "ping":
 		return "+PONG\r\n"
 	}
